26a11c34e8c804c1ee131a08fbeea93613abc6ab
zethra <jediben97@gmail.com>
2019-05-24 20:39:48 -0400

    Moved frecency stuff to db module
    Signed-off-by: zethra <jediben97@gmail.com>

--- src/desktop_entry.rs
+++ src/desktop_entry.rs
@@ -2,12 +2,14 @@ use ini::Ini;
 use failure::{Error, Fail};
 use super::App;
 
+#[allow(dead_code)]
 #[derive(Debug, Fail)]
 pub (super) enum DesktopEntryParseError {
     #[fail(display = "Desktop file is missing 'Desktop Entry' section")]
     MissingSection,
 }
 
+#[allow(dead_code)]
 pub (super) fn parse_desktop_file(path: &str) -> Result<App, Error> {
     // TODO Finish implementation
     let file = Ini::load_from_file(path)?;
