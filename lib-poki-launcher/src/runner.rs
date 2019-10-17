d27512d2f346a963d13c64408e2e54c7e8eb10ae
zethra <benaagoldberg@gmail.com>
2019-10-17 18:09:20 -0400

    Put subproccesses in their own pgroup
    Signed-off-by: zethra <benaagoldberg@gmail.com>

--- lib-poki-launcher/src/runner.rs
+++ lib-poki-launcher/src/runner.rs
@@ -1,5 +1,7 @@
 use failure::Error;
+use nix::unistd::{getpid, setpgid};
 use poki_launcher_x11::forground;
+use std::os::unix::process::CommandExt as _;
 use std::process::{Command, Stdio};
 
 use super::App;
@@ -12,13 +14,22 @@ fn parse_exec<'a>(exec: &'a str) -> (&'a str, Vec<&'a str>) {
 }
 
 impl App {
-    #[allow(dead_code)]
     pub fn run(&self) -> Result<(), Error> {
         let (cmd, args) = parse_exec(&self.exec);
-        let _child = Command::new(&cmd)
+        let mut command = Command::new(&cmd);
+        command
             .args(&args)
             .stdout(Stdio::null())
-            .spawn()?;
+            .stderr(Stdio::null());
+        unsafe {
+            command.pre_exec(|| {
+                let pid = getpid();
+                // TODO Hanle error here
+                setpgid(pid, pid).expect("Failed to set pgid");
+                Ok(())
+            });
+        }
+        let _child = command.spawn()?;
         forground(cmd);
         Ok(())
     }
